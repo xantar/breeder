50e19655d0ff9fb609631be9b0eb3ab04e5a0e51
Paul Benson <xantarnet@gmail.com>
2012-06-25 10:20:26 -0700

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breeder::Application.config.secret_token = 'cbcc9acfc14a5373e131fa823824ca87eba1336003e529e3bb988cef9d39561f2d8c51435074e43ac0a5203dc7bdd33123520d8e6e0ce8a5d609d5b7fea6363e'
